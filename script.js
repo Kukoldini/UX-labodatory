b66125daeb8f4251c8b6c9b955bec8e9e882b22d
Kukoldini <130656294+Kukoldini@users.noreply.github.com>
2023-08-15 15:39:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("asddsa");
+let button = document.querySelectorAll('.button');
+let popupMenu = document.querySelector('.popup-menu');
+let closeIcon = document.querySelector('.close-icon');
+let overlay = document.querySelector('.overlay');
+
+for (var i = 0; i < 2; i++) {
+	let currentButton = button[i];
+
+	currentButton.addEventListener('click', function() {
+		popupMenu.classList.add('show');
+		overlay.classList.add('show-overlay');
+		popupMenu.classList.remove('hide');
+		overlay.classList.remove('hide-overlay')
+	})
+}
+closeIcon.addEventListener('click', function() {
+	popupMenu.classList.add('hide');
+	overlay.classList.add('hide-overlay');
+	popupMenu.classList.remove('show');
+	overlay.classList.remove('show-overlay');
+})
+
